9ac9846332de77b06c542850db7fd2e82e25470a
Abraham galindo <abraham.garo@gmail.com>
2025-03-22 15:54:25 -0600

    feat: add logs for e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,8 @@ import { defineConfig, devices } from '@playwright/test'
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+const appURL = 'http://localhost:3000'
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -26,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: appURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -70,9 +72,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run build:e2e && npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run build:e2e && npm run start',
+    url: appURL,
+    reuseExistingServer: !process.env.CI,
+    stdout: 'pipe',
+    stderr: 'pipe',
+  },
 })
